9487463af47f140efd24ac816683d46f5d32fbfc
Luca Marino <marino.luca07@gmail.com>
2019-05-26 23:46:05 +0200

    Insert print_menu() function and interactive choise with while cycle

--- sinistri.py
+++ sinistri.py
@@ -1,5 +1,16 @@
 import csv
 
+def print_menu(dizionario):
+    print(' '*50)
+    print(' '*50)
+    print(f'{"*"*20}MENU{"*"*20}')
+    print(' '*50)
+    print(' '*50)
+    for key in dizionario.keys():
+        print(f'Inserisci {key} se vuoi filtrare per {dizionario[key]}')
+    print(f'Inserisci ESC per uscire')
+    print(' '*50)
+
 incidenti_dict = {
     'C': 'Incidente con danni materiali ai mezzi',
     'F': 'Incidente con feriti',
@@ -19,38 +30,38 @@ inc_sing_plur = {
     2: 'incidenti'
 }
 
+print_menu(incidenti_dict)
 
-print(f'{"*"*20}MENU{"*"*20}')
-print(' '*50)
+while True:
+    scelta = ''
+    while scelta not in (incidenti_dict.keys()) and scelta.upper() != 'ESC':
+        scelta = str(input('--> ')).upper()
+        
+        if scelta in (incidenti_dict.keys()) or scelta.upper() == 'ESC':
+            break
+        else:
+            print('Scelta errata, riprova!')
 
-for key in incidenti_dict.keys():
-    print(f'Inserisci {key} se vuoi filtrare per {incidenti_dict[key]}')
-print(f'Inserisci ESC per uscire')
-
-print(' '*50)
-scelta = str(input('--> ')).upper()
-print(' '*50)
-with open("./sinistri.csv", newline="") as filecsv:
-    lettore = csv.reader(filecsv, delimiter=";")
-    dati = [(linea) for linea in lettore if linea[3] == scelta]
-
-    if len(dati) == 1:
-        sing_plur = 1
+    if scelta.upper() == 'ESC':
+        print('Fine')
+        break
     else:
-        sing_plur = 2
+        print(' '*50)
+        with open("./sinistri.csv", newline="") as filecsv:
+            lettore = csv.reader(filecsv, delimiter=";")
+            dati = [(linea) for linea in lettore if linea[3] == scelta]
 
+            if len(dati) == 1:
+                sing_plur = 1
+            else:
+                sing_plur = 2
 
-    
+            for incidente in dati:
+                print(f"{incidenti_dict[incidente[3]]} avvenuto il {incidente[0]} alle \
+        ore {incidente[1]} in {incidente[2]}")
 
-
-    for incidente in dati:
-        print(f"{incidenti_dict[incidente[3]]} avvenuto il {incidente[0]} alle \
-ore {incidente[1]} in {incidente[2]}")
-
-print(' '*50)
-print('-'*50)
-print(' '*50)
-
-print(f'Ci sono stati {len(dati)} {inc_sing_plur[sing_plur]} {incidenti_dict2[scelta]} nel 2018')
-
-    # print()
+        print(' '*50)
+        print('-'*50)
+        print(' '*50)
+        print(f'Ci sono stati {len(dati)} {inc_sing_plur[sing_plur]} {incidenti_dict2[scelta]} nel 2018')
+        print_menu(incidenti_dict)
